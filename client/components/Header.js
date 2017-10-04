d351b6582ad1b1a17bd224a13fd64d06a163b663
Joe Saraceno <honestcomrade@gmail.com>
2017-10-03 20:49:16 -0700

    attach cookies to user req

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import query from '../queries/currentUser';
+
+const getUser = (user) => {
+  while (!this.props.data.loading) {
+
+  }
+};
 
 class Header extends Component {
+
   render() {
+    console.log(this.props.data);
     return(
       <div>
         Header
@@ -10,4 +20,4 @@ class Header extends Component {
   }
 };
 
-export default Header;
+export default graphql(query)(Header);
